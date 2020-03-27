da5593390dbac8cc7f431f62becb25a6f642f07e
He-xianj <474331480@qq.com>
2020-03-27 20:29:16 +0800

    网上教学3

---
+++ src/com/_3/_2/hkt.java
@@ -0,0 +1,21 @@
+package com._3._2;
+import java.util.Scanner;
+
+/**
+ * 
+ * */
+public class hkt {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String t=scanner.next();//输入
+		scanner.close();
+		try {
+			int n=Integer.parseInt(t, 16);//转换
+			System.out.println(n);//输出
+		}catch(NumberFormatException e) {
+			System.out.println("异常");//输出
+		}
+		
+	}
+
+}
