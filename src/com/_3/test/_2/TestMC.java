6c926ac1e4b0a1a6d23ea50a002e82e51184f58b
He-xianj <474331480@qq.com>
2020-03-27 19:52:50 +0800

    第3周实验(训)报告

--- src/com/_3/test/_2/TestMC.java
+++ src/com/_3/test/_2/TestMC.java
@@ -8,7 +8,9 @@ public class TestMC {
 	public static void main(String[] args) {
 		try{
 			System.out.println("====成绩统计====");
-			Scanner scan=new Scanner(System.in).useDelimiter("\r\n");
+			Scanner scanner = new Scanner(System.in);
+			Scanner scan=scanner.useDelimiter("\r\n");
+			scanner.close();
 //扫描器对象扫描的内容以回车换行作为分隔符，排除默认的空格，因而允许扫描内容包含空格
 			System.out.println("请输入要计算的一系列数据(逗号分隔):");
 			String str=scan.next();  //输入一行，如1，2，3，5，6，4
