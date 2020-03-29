e8fb0f4a904809e9cce42f89b42eb9a332c44d6a
He-xianj <474331480@qq.com>
2020-03-29 18:21:10 +0800

    第3周实验(训)报告

--- src/com/test/Information.java
+++ src/com/test/Information.java
@@ -4,7 +4,11 @@ public class Information {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Holle");
+		char charc='4';
+		char charb='5';
+		int c=charc-'0';
+		int b=charb-'0';
+		System.out.println("Holle"+(c+b));
 	}
 
 }
