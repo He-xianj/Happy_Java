b68a11cc64dc10755d0223a37448fd9ddc8a2785
He-xianj <474331480@qq.com>
2020-08-19 22:54:15 +0800

    第四次作业更新

--- src/com/frist_semester/_4/HanXinDianBing.java
+++ src/com/frist_semester/_4/HanXinDianBing.java
@@ -1,9 +1,9 @@
 package com.frist_semester._4;
-
+/**
+ * 韩信点兵，不足百人。三人一行多一人，七人一行少二人，五人一行刚刚好。问有多少人。
+ */
 public class HanXinDianBing {
-	/**
-	 * 韩信点兵，不足百人。三人一行多一人，七人一行少二人，五人一行刚刚好。问有多少人。
-	 */
+	
 	public static void main(String[] args) {
 		forBing();
 		whileBing();
